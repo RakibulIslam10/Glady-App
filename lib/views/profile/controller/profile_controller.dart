8ac42084a0cbcd3acfe97bfd6bae9d449419913b
RakibulIslam10 <rakibulislam10.net@gmail.com>
2025-12-28 18:00:09 +0600

    All Doctors

--- lib/views/profile/controller/profile_controller.dart
+++ lib/views/profile/controller/profile_controller.dart
@@ -2,7 +2,7 @@ import '../../../core/utils/basic_import.dart';
 
 class ProfileController extends GetxController {
   List<Map<String, dynamic>> profileList = [
-    {'title': 'Account Settings', 'icon': Icons.arrow_forward_ios},
+    {'title': 'Account Settings', 'icon': Icons.settings},
     {'title': 'Contact & Support', 'icon': Icons.support_agent},
     {'title': 'Terms & Conditions', 'icon': Icons.terminal_sharp},
     {'title': 'Privacy Policy', 'icon': Icons.policy},
