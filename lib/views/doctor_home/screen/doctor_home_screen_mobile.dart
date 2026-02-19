69ee5849e563fd20bb696f0512c6c47f2dd8704a
RakibulIslam10 <rakibulislam10.net@gmail.com>
2026-02-19 12:26:47 +0600

    manage

--- lib/views/doctor_home/screen/doctor_home_screen_mobile.dart
+++ lib/views/doctor_home/screen/doctor_home_screen_mobile.dart
@@ -14,113 +14,122 @@ class DoctorHomeScreenMobile extends GetView<DoctorHomeController> {
         child: Obx(
           () => controller.isLoading.value
               ? LoadingWidget()
-              : ListView(
-                  physics: BouncingScrollPhysics(),
-                  padding: Dimensions.defaultHorizontalSize.edgeHorizontal,
-                  children: [
-                    Space.height.v20,
-                    const DoctorAppointmentSectionWidget(),
-                    Space.height.v20,
-                    Container(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: Dimensions.defaultHorizontalSize,
-                      ),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(Dimensions.radius),
-                        border: Border.all(color: CustomColors.borderColor),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: mainSpaceBet,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.symmetric(
-                              horizontal:
-                                  Dimensions.defaultHorizontalSize * 0.5,
-                              vertical: Dimensions.verticalSize * 0.5,
-                            ),
-                            child: Column(
-                              crossAxisAlignment: crossStart,
-                              children: [
-                                TextWidget(
-                                  "Booking Request",
-                                  color: CustomColors.grayShade,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                                TextWidget(
-                                  '${controller.dashboardModel.value?.data.stats.bookingRequestCount ?? 0}',
-                                ),
-                              ],
-                            ),
+              : RefreshIndicator(
+                  color: CustomColors.primary,
+                  backgroundColor: CustomColors.whiteColor,
+                  onRefresh: () async {
+                    controller.fetchDashboard();
+                  },
+                  child: ListView(
+                    physics: BouncingScrollPhysics(),
+                    padding: Dimensions.defaultHorizontalSize.edgeHorizontal,
+                    children: [
+                      Space.height.v20,
+                      const DoctorAppointmentSectionWidget(),
+                      Space.height.v20,
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: Dimensions.defaultHorizontalSize,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(
+                            Dimensions.radius,
                           ),
-                          Container(
-                            width: 1,
-                            height: 60.h,
-                            color: Colors.grey.withOpacity(0.5),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(
-                              horizontal:
-                                  Dimensions.defaultHorizontalSize * 0.5,
-                              vertical: Dimensions.verticalSize * 0.5,
+                          border: Border.all(color: CustomColors.borderColor),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: mainSpaceBet,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                horizontal:
+                                    Dimensions.defaultHorizontalSize * 0.5,
+                                vertical: Dimensions.verticalSize * 0.5,
+                              ),
+                              child: Column(
+                                crossAxisAlignment: crossStart,
+                                children: [
+                                  TextWidget(
+                                    "Booking Request",
+                                    color: CustomColors.grayShade,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                  TextWidget(
+                                    '${controller.dashboardModel.value?.data.stats.bookingRequestCount ?? 0}',
+                                  ),
+                                ],
+                              ),
                             ),
-                            child: Column(
-                              crossAxisAlignment: crossEnd,
-                              children: [
-                                TextWidget(
-                                  "Accepted",
-                                  color: CustomColors.grayShade,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                                TextWidget(
-                                  '${controller.dashboardModel.value?.data.stats.acceptedCount ?? 0}',
-                                ),
-                              ],
+                            Container(
+                              width: 1,
+                              height: 60.h,
+                              color: Colors.grey.withOpacity(0.5),
                             ),
-                          ),
-                        ],
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                horizontal:
+                                    Dimensions.defaultHorizontalSize * 0.5,
+                                vertical: Dimensions.verticalSize * 0.5,
+                              ),
+                              child: Column(
+                                crossAxisAlignment: crossEnd,
+                                children: [
+                                  TextWidget(
+                                    "Accepted",
+                                    color: CustomColors.grayShade,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                  TextWidget(
+                                    '${controller.dashboardModel.value?.data.stats.acceptedCount ?? 0}',
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    Space.height.v20,
-                    ListView.builder(
-                      itemCount:
-                          controller
+                      Space.height.v20,
+                      ListView.builder(
+                        itemCount:
+                            controller
+                                .dashboardModel
+                                .value
+                                ?.data
+                                .upcomingAppointments
+                                .length ??
+                            0,
+                        addRepaintBoundaries: true,
+                        cacheExtent: 500,
+                        shrinkWrap: true,
+                        primary: false,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          final appointment = controller
                               .dashboardModel
-                              .value
-                              ?.data
-                              .upcomingAppointments
-                              .length ??
-                          0,
-                      addRepaintBoundaries: true,
-                      cacheExtent: 500,
-                      shrinkWrap: true,
-                      primary: false,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemBuilder: (context, index) {
-                        final appointment = controller
-                            .dashboardModel
-                            .value!
-                            .data
-                            .upcomingAppointments[index];
-                        return Padding(
-                          padding: EdgeInsets.only(
-                            bottom: Dimensions.heightSize,
-                          ),
-                          child: RequestCard(
-                            name: appointment.patientName,
-                            service: appointment.reasonTitle,
-                            time: appointment.timeRange,
-                            status: DateFormat(
-                              "dd MMMM",
-                            ).format(DateTime.parse(appointment.date)),
-                            buttonTitle: 'View',
-                            cardOnTap: () {
-                              Get.toNamed(Routes.appointmentDetailsScreen);
-                            },
-                          ),
-                        );
-                      },
-                    ),
-                  ],
+                              .value!
+                              .data
+                              .upcomingAppointments[index];
+                          return Padding(
+                            padding: EdgeInsets.only(
+                              bottom: Dimensions.heightSize,
+                            ),
+                            child: RequestCard(
+                              name: appointment.patientName,
+                              service: appointment.reasonTitle,
+                              time: appointment.timeRange,
+                              status: DateFormat(
+                                "dd MMMM",
+                              ).format(DateTime.parse(appointment.date)),
+                              buttonTitle: 'View',
+                              cardOnTap: () {
+                                Get.toNamed(Routes.appointmentDetailsScreen);
+                              },
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
                 ),
         ),
       ),
