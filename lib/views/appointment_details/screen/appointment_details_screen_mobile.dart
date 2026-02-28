bdc6e4e834d104129d2756f992decc11c8105edf
RakibulIslam10 <rakibulislam10.net@gmail.com>
2026-02-28 15:51:57 +0600

    manage

--- lib/views/appointment_details/screen/appointment_details_screen_mobile.dart
+++ lib/views/appointment_details/screen/appointment_details_screen_mobile.dart
@@ -6,126 +6,99 @@ class AppointmentDetailsScreenMobile
 
   @override
   Widget build(BuildContext context) {
-    final appointmentData = controller.userAppointmentDetailsModel.value;
-    final status = appointmentData?.data.status;
-    final doctorAppointmentData =
-        controller.doctorAppointmentDetailsModel.value;
     return Scaffold(
-      bottomNavigationBar: Obx(
-        () => controller.isLoading.value
-            ? SizedBox.shrink()
-            : SafeArea(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: Dimensions.defaultHorizontalSize,
-                    vertical: Dimensions.verticalSize * 0.5,
-                  ),
-                  child: Column(
-                    mainAxisSize: mainMin,
-                    children: [
-                      Row(
-                        children: [
-                          if (AppStorage.isUser != 'USER') ...[
-                            if (controller
-                                    .doctorAppointmentDetailsModel
-                                    .value
-                                    ?.data
-                                    .status !=
-                                'CANCELLED')
-                              Expanded(
-                                child: PrimaryButtonWidget(
-                                  outlineButton: true,
-                                  buttonTextColor: CustomColors.rejected,
-                                  borderColor: CustomColors.rejected,
-                                  borderWidth: 1.5,
-                                  onPressed: () {
-                                    showModalBottomSheet(
-                                      context: context,
-                                      builder: (context) => BottomSheetDialogWidget(
-                                        titleColor: CustomColors.rejected,
-                                        isInputField: true,
-                                        title: 'Reject Request',
-                                        inputController:
-                                            controller.reasonController,
-                                        subTitle:
-                                            'write a reason for rejecting the appointment request',
-                                        isLoading: controller.isDeleting,
-                                        buttonTex: 'Reject',
-                                        action: () {
-                                          controller.rejectAppointment();
-                                        },
-                                      ),
-                                    );
-                                  },
-                                  title: 'Reject Appointment',
-                                ),
-                              ),
-                            Space.width.v10,
-                          ],
-                          Builder(
-                            builder: (_) {
-                              final currentStatus = AppStorage.isUser == 'USER'
-                                  ? controller
-                                        .userAppointmentDetailsModel
-                                        .value
-                                        ?.data
-                                        .status
-                                  : controller
-                                        .doctorAppointmentDetailsModel
-                                        .value
-                                        ?.data
-                                        .status;
+      bottomNavigationBar: Obx(() {
+        if (controller.isLoading.value) return SizedBox.shrink();
 
-                              return Expanded(
-                                child: GestureDetector(
-                                  onTap: currentStatus == 'CANCELLED'
-                                      ? () {}
-                                      : currentStatus == 'ONGOING'
-                                      ? () => Get.toNamed(Routes.videoCallScreen)
-                                      : () => Get.toNamed(Routes.inboxScreen),
-                                  child: Container(
-                                    padding: EdgeInsets.symmetric(
-                                      horizontal:
-                                          Dimensions.defaultHorizontalSize *
-                                          0.5,
-                                      vertical: Dimensions.verticalSize * 0.55,
-                                    ),
-                                    decoration: BoxDecoration(
-                                      color: currentStatus == 'CANCELLED'
-                                          ? CustomColors.rejected
-                                          : CustomColors.primary,
-                                      borderRadius: BorderRadius.circular(
-                                        Dimensions.radius,
-                                      ),
-                                    ),
-                                    child: Center(
-                                      child: TextWidget(
-                                        currentStatus == 'CANCELLED'
-                                            ? 'Cancelled'
-                                            : currentStatus == 'ONGOING' ? 'Join' : 'Chat',
-                                        color: CustomColors.whiteColor,
-                                        fontSize: Dimensions.titleMedium,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                        ],
+        final currentStatus = AppStorage.isUser == 'USER'
+            ? controller.userAppointmentDetailsModel.value?.data.status
+            : controller.doctorAppointmentDetailsModel.value?.data.status;
+
+        return SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: Dimensions.defaultHorizontalSize,
+              vertical: Dimensions.verticalSize * 0.5,
+            ),
+            child: Column(
+              mainAxisSize: mainMin,
+              children: [
+                Row(
+                  children: [
+                    if (AppStorage.isUser != 'USER') ...[
+                      Expanded(
+                        child: PrimaryButtonWidget(
+                          outlineButton: true,
+                          buttonTextColor: CustomColors.rejected,
+                          borderColor: CustomColors.rejected,
+                          borderWidth: 1.5,
+                          onPressed: () {
+                            showModalBottomSheet(
+                              context: context,
+                              builder: (context) => BottomSheetDialogWidget(
+                                titleColor: CustomColors.rejected,
+                                isInputField: true,
+                                title: 'Reject Request',
+                                inputController: controller.reasonController,
+                                subTitle:
+                                'write a reason for rejecting the appointment request',
+                                isLoading: controller.isDeleting,
+                                buttonTex: 'Reject',
+                                action: () {
+                                  controller.rejectAppointment();
+                                },
+                              ),
+                            );
+                          },
+                          title: 'Reject Appointment',
+                        ),
                       ),
+                      Space.width.v10,
                     ],
-                  ),
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: currentStatus == 'CANCELLED'
+                            ? () {}
+                            : () => Get.toNamed(Routes.inboxScreen),
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: Dimensions.defaultHorizontalSize * 0.5,
+                            vertical: Dimensions.verticalSize * 0.55,
+                          ),
+                          decoration: BoxDecoration(
+                            color: currentStatus == 'CANCELLED'
+                                ? CustomColors.rejected
+                                : CustomColors.primary,
+                            borderRadius:
+                            BorderRadius.circular(Dimensions.radius),
+                          ),
+                          child: Center(
+                            child: TextWidget(
+                              currentStatus == 'CANCELLED'
+                                  ? 'Cancelled'
+                                  : 'Chat',
+                              color: CustomColors.whiteColor,
+                              fontSize: Dimensions.titleMedium,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-      ),
+              ],
+            ),
+          ),
+        );
+      }),
       appBar: CommonAppBar(title: "Details"),
       body: SafeArea(
         child: Obx(() {
           if (controller.isLoading.value) {
             return LoadingWidget();
           }
+
           if (AppStorage.isUser == 'USER') {
             final appointmentData =
                 controller.userAppointmentDetailsModel.value;
@@ -146,8 +119,6 @@ class AppointmentDetailsScreenMobile
               physics: BouncingScrollPhysics(),
               children: [
                 Space.height.v15,
-
-                // ✅ Doctor Card
                 DoctorDetailsCard(
                   imageUrl: doctor.profileImage,
                   name: doctor.name,
@@ -158,7 +129,6 @@ class AppointmentDetailsScreenMobile
                   startingPrice: data.consultationFee.toDouble(),
                   onTap: () {},
                 ),
-
                 TextWidget(
                   padding: EdgeInsets.symmetric(
                     vertical: Dimensions.verticalSize,
@@ -167,17 +137,14 @@ class AppointmentDetailsScreenMobile
                   fontWeight: FontWeight.bold,
                   fontSize: Dimensions.titleMedium,
                 ),
-
                 InfoPairRow(
                   leftTitle: 'Reason',
                   leftValue: data.reasonTitle,
                   rightTitle: 'Booking Date',
-                  rightValue: DateFormat(
-                    "dd MMM yyyy",
-                  ).format(data.appointmentDate.toLocal()),
+                  rightValue: DateFormat("dd MMM yyyy")
+                      .format(data.appointmentDate.toLocal()),
                 ),
                 Space.height.v15,
-
                 TextWidget(
                   padding: EdgeInsets.only(
                     top: Dimensions.heightSize,
@@ -187,25 +154,21 @@ class AppointmentDetailsScreenMobile
                   fontSize: Dimensions.titleSmall,
                   fontWeight: FontWeight.bold,
                 ),
-
                 TextWidget(
                   textAlign: TextAlign.justify,
                   fontSize: Dimensions.titleSmall,
                   fontWeight: FontWeight.w400,
                   data.reasonDetails,
                 ),
-
                 Space.height.v15,
-
                 InfoPairRow(
                   leftTitle: 'Visiting Date',
                   leftValue:
-                      '${DateFormat("dd MMM yyyy").format(data.appointmentDate.toLocal())}\n${data.appointmentTime}',
+                  '${DateFormat("dd MMM yyyy").format(data.appointmentDate.toLocal())}\n${data.appointmentTime}',
                   rightTitle: 'Status',
                   rightValue: data.status,
                 ),
                 Space.height.v15,
-
                 TextWidget("Appointment Fee", fontWeight: FontWeight.w400),
                 TextWidget(
                   '₦${data.consultationFee}',
@@ -213,7 +176,6 @@ class AppointmentDetailsScreenMobile
                   fontSize: Dimensions.titleLarge,
                 ),
                 Space.height.v15,
-
                 if (data.attachments.isNotEmpty) ...[
                   TextWidget(
                     padding: EdgeInsets.only(bottom: Dimensions.heightSize),
@@ -225,21 +187,19 @@ class AppointmentDetailsScreenMobile
                     runSpacing: Dimensions.heightSize,
                     children: List.generate(
                       data.attachments.length,
-                      (index) => Container(
+                          (index) => Container(
                         height: 100.h,
                         width: 100.w,
                         decoration: BoxDecoration(
                           border: Border.all(
                             color: CustomColors.grayShade.withOpacity(0.7),
                           ),
-                          borderRadius: BorderRadius.circular(
-                            Dimensions.radius,
-                          ),
+                          borderRadius:
+                          BorderRadius.circular(Dimensions.radius),
                         ),
                         child: ClipRRect(
-                          borderRadius: BorderRadius.circular(
-                            Dimensions.radius,
-                          ),
+                          borderRadius:
+                          BorderRadius.circular(Dimensions.radius),
                           child: CachedNetworkImage(
                             imageUrl: data.attachments[index].url,
                             fit: BoxFit.cover,
@@ -276,21 +236,17 @@ class AppointmentDetailsScreenMobile
               physics: BouncingScrollPhysics(),
               children: [
                 Space.height.v15,
-
-                // ✅ Patient Info
                 PatientInfoWidgetWithAsset(
                   patientImageNetwork: patient.profileImage,
                   patientName: patient.name,
-                  dateOfBirth: DateFormat(
-                    "dd MMM yyyy",
-                  ).format(patient.dateOfBirth.toLocal()),
+                  dateOfBirth: DateFormat("dd MMM yyyy")
+                      .format(patient.dateOfBirth.toLocal()),
                   phoneNumber: patient.phone,
                   bloodGroup: patient.bloodGroup,
                   allergies: patient.allergies.isNotEmpty
                       ? patient.allergies
                       : ['None'],
                 ),
-
                 TextWidget(
                   padding: EdgeInsets.symmetric(
                     vertical: Dimensions.verticalSize,
@@ -299,17 +255,14 @@ class AppointmentDetailsScreenMobile
                   fontWeight: FontWeight.bold,
                   fontSize: Dimensions.titleMedium,
                 ),
-
                 InfoPairRow(
                   leftTitle: 'Reason',
                   leftValue: data.reasonTitle,
                   rightTitle: 'Booking Date',
-                  rightValue: DateFormat(
-                    "dd MMM yyyy",
-                  ).format(data.appointmentDate.toLocal()),
+                  rightValue: DateFormat("dd MMM yyyy")
+                      .format(data.appointmentDate.toLocal()),
                 ),
                 Space.height.v15,
-
                 TextWidget(
                   padding: EdgeInsets.only(
                     top: Dimensions.heightSize,
@@ -319,25 +272,21 @@ class AppointmentDetailsScreenMobile
                   fontSize: Dimensions.titleSmall,
                   fontWeight: FontWeight.bold,
                 ),
-
                 TextWidget(
                   textAlign: TextAlign.justify,
                   fontSize: Dimensions.titleSmall,
                   fontWeight: FontWeight.w400,
                   data.reasonDetails,
                 ),
-
                 Space.height.v15,
-
                 InfoPairRow(
                   leftTitle: 'Visiting Date',
                   leftValue:
-                      '${DateFormat("dd MMM yyyy").format(data.appointmentDate.toLocal())}\n${data.appointmentTime}',
+                  '${DateFormat("dd MMM yyyy").format(data.appointmentDate.toLocal())}\n${data.appointmentTime}',
                   rightTitle: 'Status',
                   rightValue: data.status,
                 ),
                 Space.height.v15,
-
                 TextWidget("Appointment Fee", fontWeight: FontWeight.w400),
                 TextWidget(
                   '₦${data.consultationFee}',
@@ -345,7 +294,6 @@ class AppointmentDetailsScreenMobile
                   fontSize: Dimensions.titleLarge,
                 ),
                 Space.height.v15,
-
                 if (data.attachments.isNotEmpty) ...[
                   TextWidget(
                     padding: EdgeInsets.only(bottom: Dimensions.heightSize),
@@ -357,21 +305,19 @@ class AppointmentDetailsScreenMobile
                     runSpacing: Dimensions.heightSize,
                     children: List.generate(
                       data.attachments.length,
-                      (index) => Container(
+                          (index) => Container(
                         height: 100.h,
                         width: 100.w,
                         decoration: BoxDecoration(
                           border: Border.all(
                             color: CustomColors.grayShade.withOpacity(0.7),
                           ),
-                          borderRadius: BorderRadius.circular(
-                            Dimensions.radius,
-                          ),
+                          borderRadius:
+                          BorderRadius.circular(Dimensions.radius),
                         ),
                         child: ClipRRect(
-                          borderRadius: BorderRadius.circular(
-                            Dimensions.radius,
-                          ),
+                          borderRadius:
+                          BorderRadius.circular(Dimensions.radius),
                           child: CachedNetworkImage(
                             imageUrl: data.attachments[index].url,
                             fit: BoxFit.cover,
